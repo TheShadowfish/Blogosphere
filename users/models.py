da51d99b653d867f44bba84ad772efef346ad61b
TheShadowfish <shadowfish@yandex.ru>
2024-10-21 01:07:06 +0300

    initial commit

---
+++ users/models.py
@@ -0,0 +1,93 @@
+from django.db import models
+
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+from users.validators import validate_email_domain, validate_password
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError("The given username must be set")
+        user = self.model(username=username, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, password, **extra_fields)
+
+class User(AbstractUser):
+    objects = UserManager()
+    """логин
+пароль
+номер
+дата рождения
+дата создания
+дата редактирования"""
+    username = models.CharField(max_length=40, unique=True, verbose_name="логин", help_text="Введите логин")
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Электронная почта",
+        help_text="Введите электронную почту, разрешены домены: mail.ru, yandex.ru",
+        validators=[validate_email_domain],
+    )
+    password = models.CharField(
+        verbose_name="Пароль",
+        help_text="Введите пароль, должен быть не менее 8 символов, должен включать цифры",
+        validators=[validate_password],
+    )
+
+
+    phone_number = models.CharField(max_length=17, verbose_name="телефон", help_text="Введите номер телефона в формате 999 123 45 67, +7 подставится автоматически",
+        unique=True)
+
+
+    birth_date = models.DateField(verbose_name="дата рождения", help_text="Введите вашу дату рождения")
+    created_at = models.DateField(auto_now_add=True, verbose_name="дата создания", help_text="Введите дату создания пользователя")
+    updated_at = models.DateField(auto_now=True, verbose_name="дата редактирования", help_text="Введите дату редактирования пользователя")
+
+
+
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
+
+    def __str__(self):
+        return f"{self.username} - ({self.email})"
+
+    def clean_phone(self):
+        # Убираем пробелы
+        phone = self.phone.replace(" ", "").replace("-", "")
+        # Добавляем код +7, если не введен
+        if not phone.startswith("+7"):
+            phone = "+7" + phone
+        return phone
+
+    def save(self, *args, **kwargs):
+        # Перед сохранением форматируем номер телефона
+        self.phone = self.clean_phone()
+        super().save(*args, **kwargs)
+
+
---
+++ users/validators.py
@@ -0,0 +1,44 @@
+from rest_framework.exceptions import ValidationError
+
+
+def validate_email_domain(value):
+    """Валидатор домена электронной почты при создании пользователя"""
+
+    allowed_domains = ["mail.ru", "yandex.ru"]
+    domain = value.split("@")[-1]
+
+    if domain not in allowed_domains:
+        raise ValidationError(f"Разрешены следующие домены {', '.join(allowed_domains)}")
+
+
+def validate_password(value):
+    """Валидатор пароля - что он длиннее 8 символов и содержит хотя бы одну цифру """
+    if len(value) < 8:
+        raise ValidationError("Пароль должен быть длиннее 8 символов")
+
+    digits = [s for s in value if s in '1234567890']
+
+    if len(digits) < 1:
+        raise ValidationError("Строка должна содержать хотя бы одну цифру")
+
+
+def validate_phone(value):
+    """Валидатор корректности введенного номера телефона"""
+
+    # убираем тире и пробелы если пользователь их ввел
+    value = value.replace(" ", "").replace("-", "")
+
+    # прибавляем '+7' если пользователь не ввел
+    if not value.startswith("+7"):
+        value = "+7" + value
+
+    # если номер слишком длинный +79876543210
+    if len(value) != 12:
+        raise ValidationError(
+            "Номер телефона должен состоять из 11 цифр, код +7 не учитывается, и подставляется автоматически."
+        )
+    # если в номере не только цифры
+    if not value[1:].isdigit():
+        raise ValidationError("Номер телефона должен содержать только цифры")
+
+    return value
